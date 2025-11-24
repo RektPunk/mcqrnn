98c5cde6e1a321aa9be23c83064cc4dfc1bb6746
unknown <rektpunk@gmail.com>
2025-11-24 17:57:21 +0900

    [Hotfix] fix typo transform -> data_transform and add ruff noqa

--- mcqrnn/__init__.py
+++ mcqrnn/__init__.py
@@ -1,5 +1,6 @@
+# ruff: noqa
+from mcqrnn.data_transform import DataTransformer
 from mcqrnn.generate_example_dataset import generate_example
-from mcqrnn.transform import DataTransformer
-from mcqrnn.models import Mcqrnn
 from mcqrnn.loss import TiltedAbsoluteLoss
+from mcqrnn.models import Mcqrnn
 from mcqrnn.train import train_step
