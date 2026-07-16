543e800d59d8158ebe525832d66152b0e66dd886
unknown <rektpunk@gmail.com>
2026-07-16 09:04:47 +0900

    docs: remove redundant comments

--- run_torch.py
+++ run_torch.py
@@ -5,30 +5,23 @@ import torch
 from mcqrnn.torch import MCQRNNRegressor
 
 if __name__ == "__main__":
-    # Set seed for reproducibility
     np.random.seed(42)
     torch.manual_seed(42)
 
-    ### train data
     n = 1000
     input_dim = 1
     x_data = np.random.uniform(-1, 1, (n, input_dim))
-
-    # np.sinc(x) is defined as sin(pi*x) / (pi*x)
     sincx = np.sinc(x_data)
     Z = sincx.reshape(n, input_dim)
-    # heteroscedastic noise
     ep = np.random.normal(0, 0.1 * np.exp(1 - x_data)).reshape(n, 1)
     y_data = Z + ep
 
-    ### test data
     x_test_data = np.random.uniform(-1, 1, (n, input_dim))
     sincx_test = np.sinc(x_test_data)
     y_test = sincx_test.reshape(n, input_dim) + np.random.normal(
         0, 0.1 * np.exp(1 - x_test_data)
     ).reshape(n, 1)
 
-    ###
     tau_vec = np.arange(0.1, 1.0, 0.1)
 
     mcqrnn_regressor = MCQRNNRegressor(
--- tests/test_tensorflow.py
+++ tests/test_tensorflow.py
@@ -5,26 +5,22 @@ from mcqrnn.tensorflow import MCQRNNRegressor
 
 
 def test_tensorflow_non_cross():
-    # Set seed for reproducibility
     np.random.seed(42)
     tf.random.set_seed(42)
 
-    ### train data
     n = 100
     input_dim = 1
-    x_data = np.random.uniform(-1, 1, (n, input_dim))
 
-    # np.sinc(x) is defined as sin(pi*x) / (pi*x)
+    x_data = np.random.uniform(-1, 1, (n, input_dim))
     sincx = np.sinc(x_data)
     Z = sincx.reshape(n, input_dim)
-    # heteroscedastic noise
     ep = np.random.normal(0, 0.1 * np.exp(1 - x_data)).reshape(n, 1)
     y_data = Z + ep
 
     tau_vec = np.arange(0.1, 1.0, 0.1)
 
     mcqrnn = MCQRNNRegressor(tau=tau_vec, out_features=3, dense_features=3, epochs=100)
-
     mcqrnn.fit(x_data, y_data)
     predictions = mcqrnn.predict(x_data)
+
     assert np.all(np.diff(predictions, axis=1) >= 0)
